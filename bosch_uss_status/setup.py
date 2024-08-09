09528735356b388d78df19ae257b93e747b2a085
FatimaPaltech <fatima.yousif@paltech.eu>
2024-08-09 16:43:02 +0200

    Add files via upload

---
+++ bosch_uss_status/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'bosch_uss_status'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='paltech',
+    maintainer_email='paltech@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'bosch_uss = bosch_uss_status.bosch_uss:main',
+        ],
+    },
+)

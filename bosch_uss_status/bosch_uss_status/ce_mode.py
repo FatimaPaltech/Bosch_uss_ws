e592522fc1f3c2f2204c679d1b99b1dbf259d90f
FatimaPaltech <fatima.yousif@paltech.eu>
2024-08-14 09:28:25 +0200

    Add files via upload

--- bosch_uss_status/bosch_uss_status/ce_mode.py
+++ bosch_uss_status/bosch_uss_status/ce_mode.py
@@ -4,24 +4,35 @@ from off_highway_uss_msgs.msg import Objects
 from std_msgs.msg import String
 from geometry_msgs.msg import Twist
 import numpy as np
+from nav_msgs.msg import Odometry
 
 class CEMOde(Node):
     def __init__(self):
-        super().__init__('ce_mode')
+        super().__init__('object_subscriber')
         
-        self.subscription = self.create_subscription(
+        self.ce_sub = self.create_subscription(
             Objects,
             '/objects',
             self.listener_callback,
             10)
+
+        self.odom_sub = self.create_subscription(
+            Odometry,
+            '/odom',
+            self.odom_callback,
+            10
+        )
         
         self.get_logger().info('Listening to /objects topic')
         
         self.vel_pub = self.create_publisher(Twist, "/osorno2/uss_vel", 10)
         self.control_publisher = self.create_publisher(String, '/robot_control', 10)
-        
-        
+           
         self.readings = []  
+    
+        self.current_linear_speed = 0.0
+        self.current_angular_speed = 0.0
+ 
 
     def listener_callback(self, msg):
         current_reading = []
@@ -55,22 +66,57 @@ class CEMOde(Node):
         
         print("---- Median distance: ", median_dist)
 
-        if 3.50 <= median_dist < 6.00:
+        if 1.00 <= median_dist < 6.00:
             slow_down = True
-        elif 0 <= median_dist < 3.50:
+        elif 0 <= median_dist < 1.00:
             stop = True
         
         if stop:
-            self.send_control_command('****************STOP*******************')
+            # self.send_control_command('****************STOP*******************')
+            self.stop_robot()
         elif slow_down:
-            self.send_control_command('****************SLOW DOWN*******************')
+            # self.send_control_command('****************SLOW DOWN*******************')
+            self.slow_robot()
         
     def send_control_command(self, command):
         msg = String()
         msg.data = command
         self.control_publisher.publish(msg)
         print(f'Sent control command: {command}')
+
+    def odom_callback(self, msg):
+        self.current_linear_speed = msg.twist.twist.linear.x
+        self.current_angular_speed = msg.twist.twist.angular.z
+    
+    def stop_robot(self):
+        print("**************************STOP ROBOT**************************")
+        msg = Twist()
+        msg.linear.x = 0.0
+        msg.linear.y = 0.0
+        msg.linear.z = 0.0
  
+        msg.angular.x = 0.0
+        msg.angular.y = 0.0
+        msg.angular.z = 0.0
+        self.vel_pub.publish(msg)
+ 
+    def slow_robot(self):
+        print("**************************SLOW DOWN ROBOT**************************")
+        msg = Twist()
+        
+        # --- next ---
+        # msg.linear.x = 0.3  
+        msg.linear.x = self.current_linear_speed * 0.5  # Slow down to 50% of the current speed
+        
+        msg.linear.y = 0.0
+        msg.linear.z = 0.0
+ 
+        msg.angular.x = 0.0
+        msg.angular.y = 0.0
+        # msg.angular.z = 0.0  
+        msg.angular.z = self.current_angular_speed * 0.5  # Slow down to 50% of the current speed
+ 
+        self.vel_pub.publish(msg)
 
 def main(args=None):
     rclpy.init(args=args)
